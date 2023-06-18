9737186509de8a37146180f1d43ae664c7a8dfa5
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-18 17:03:40 -0500

    Creates addLast method

---
+++ DataStructures/src/com/start/LinkedList.java
@@ -0,0 +1,42 @@
+package com.start;
+
+public class LinkedList {
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+
+    public void addLast(int item) {
+        var node = new Node(item);
+
+        if (first == null)
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+    }
+
+    // deleteFirst
+    // deleteLast
+    // contains
+    // indexOf
+
+    public int indexOf(int item) {
+        int index = 0;
+        var current = first;
+        while (current != null) {
+            if (current.value == item) return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+}
