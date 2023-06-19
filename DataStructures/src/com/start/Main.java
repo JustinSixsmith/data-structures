d9d9e06fed6f04012719a0e147ed542062e19122
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-19 10:37:07 -0500

    Creates Array class and implements insert and remove

--- DataStructures/src/com/start/Main.java
+++ DataStructures/src/com/start/Main.java
@@ -1,15 +1,16 @@
 package com.start;
 
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new LinkedList();
-        list.addFirst(10);
-        list.addLast(20);
-        list.addLast(30);
-
-        var array = list.toArray();
-        System.out.println(Arrays.toString(array));
+        var numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.print();
+//        numbers.removeAt(3);
+//        numbers.print();
+//        System.out.println(numbers.indexOf(1));
     }
 }
