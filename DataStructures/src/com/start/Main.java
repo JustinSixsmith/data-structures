30263adfa1ec7a8f12e4e969e2ed8b11c0862676
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-18 19:07:16 -0500

    Implements toArray method

--- DataStructures/src/com/start/Main.java
+++ DataStructures/src/com/start/Main.java
@@ -1,25 +1,15 @@
 package com.start;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-//        list.addLast(10);
-//        list.addLast(20);
-//        list.addLast(30);
-//        System.out.println(list.contains(10));
-//        System.out.println(list.size());
-//        var array = list.toArray();
-//        System.out.println(Arrays.toString(array));
-
         var list = new LinkedList();
-        System.out.println(list.size());
         list.addFirst(10);
-        System.out.println(list.size());
         list.addLast(20);
         list.addLast(30);
-        System.out.println(list.size());
 
-        list.removeLast();
-        list.removeFirst();
-        System.out.println(list.size());
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
     }
 }
