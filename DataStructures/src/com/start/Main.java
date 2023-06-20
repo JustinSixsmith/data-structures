b9f3b342a85d3183c96d1b7252b4ab40285ed0ed
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-20 07:38:17 -0500

    Implements Stack using an Array

--- DataStructures/src/com/start/Main.java
+++ DataStructures/src/com/start/Main.java
@@ -1,17 +1,11 @@
 package com.start;
 
-
-import com.codewithmosh.StringReverser;
-
-import java.util.Arrays;
-import java.util.Stack;
-
 public class Main {
     public static void main(String[] args) {
-        String str = "(1 + 2)";
-
-        Expression exp = new Expression();
-        var result = exp.isBalanced(str);
-        System.out.println(result);
+        Stack stack = new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack.peek());
     }
 }
