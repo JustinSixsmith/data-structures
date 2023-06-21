9d35ab2816be25c0b9e5e14166f47f4dfa6dd9e0
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-20 21:32:53 -0500

    Refactors HashTable methods

--- DataStructures/src/com/start/HashTable.java
+++ DataStructures/src/com/start/HashTable.java
@@ -16,43 +16,43 @@ public class HashTable {
     private LinkedList<Entry>[] entries = new LinkedList[5];
 
     public void put(int key, String value) {
-        var index = hash(key);
-        if (entries[index] == null)
-            entries[index] = new LinkedList<>();
-
-        var bucket = entries[index];
-        for (var entry : bucket) {
-            if (entry.key == key) {
-                entry.value = value;
-                return;
-            }
+        var entry = getEntry(key);
+        if (entry != null) {
+            entry.value = value;
+            return;
         }
 
-        bucket.addLast(new Entry(key, value));
+        getOrCreateBucket(key).add(new Entry(key, value));
     }
 
     public String get(int key) {
-        var index = hash(key);
-        var bucket = entries[index];
-        if (bucket != null) {
-            for (var entry : bucket)
-                if (entry.key == key)
-                    return entry.value;
-        }
-        return null;
+        var entry = getEntry(key);
+        return (entry == null) ? null : entry.value;
     }
 
     public void remove(int key) {
-        var index = hash(key);
-        var bucket = entries[index];
-        if (bucket == null)
+        var entry = getEntry(key);
+        if (entry == null)
             throw new IllegalStateException();
-        for (var entry : bucket)
-            if (entry.key == key) {
-                bucket.remove(entry);
-                return;
-            }
-        throw new IllegalStateException();
+        getBucket(key).remove(entry);
+    }
+
+    private LinkedList<Entry> getBucket(int key) {
+        return entries[hash(key)];
+    }
+
+    private LinkedList<Entry> getOrCreateBucket(int key) {
+        var bucket = entries[hash(key)];
+        return (bucket != null) ? bucket : new LinkedList<>();
+    }
+
+    private Entry getEntry(int key) {
+        var bucket = getBucket(key);
+        if (bucket != null)
+            for (var entry : bucket)
+                if (entry.key == key)
+                    return entry;
+        return null;
     }
 
     private int hash(int key) {
